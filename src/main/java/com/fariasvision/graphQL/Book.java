d02ad25355d5e823c37a78926e2bfae59d2475e7
caiofariaas <caiofarias077@gmail.com>
2024-11-28 15:25:47 -0300

    :sparkles:feat: graphiql

---
+++ src/main/java/com/fariasvision/graphQL/Book.java
@@ -0,0 +1,17 @@
+package com.fariasvision.graphQL;
+
+import java.util.Arrays;
+import java.util.List;
+
+public record Book(
+        Integer id,
+        String name,
+        Integer pageCount
+) {
+    public static List<Book> books = Arrays.asList(
+            new Book(1, "Jean-Michel Basquiat. 40th Ed.", 604),
+            new Book(2, "Pharrell-Isms: 13", 300),
+            new Book(3, "Virgil Abloh. Icons", 344),
+            new Book(4, "Abloh-Isms: 6", 233)
+    );
+}
